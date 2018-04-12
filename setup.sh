722780a61a1c3dbde301867fadb36cc9e224c205
dariober <dario.beraldi@gmail.com>
2018-04-12 17:11:35 +0100

    First basic scripts

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+cwd=`pwd`
+
+mkdir -p downloads
+mkdir -p bin
+
+cd downloads
+
+wget https://github.com/arq5x/bedtools2/releases/download/v2.27.1/bedtools-2.27.1.tar.gz 
+tar xf bedtools-2.27.1.tar.gz 
+rm bedtools-2.27.1.tar.gz
+cd bedtools2
+make
+cp bin/* ${cwd}/bin/
+
+cd ${cwd}
