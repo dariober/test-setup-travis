ccaff3123264139dc149b359a350075e07d5a688
dariober <dario.beraldi@gmail.com>
2018-04-13 09:05:42 +0100

    Test install snakemake

--- setup.sh
+++ setup.sh
@@ -10,13 +10,15 @@ mkdir -p bin
 
 cd downloads
 
-wget https://github.com/arq5x/bedtools2/releases/download/v2.27.1/bedtools-2.27.1.tar.gz 
-tar xf bedtools-2.27.1.tar.gz 
-rm bedtools-2.27.1.tar.gz
-cd bedtools2
-make
-cp bin/* ${cwd}/bin/
+#wget https://github.com/arq5x/bedtools2/releases/download/v2.27.1/bedtools-2.27.1.tar.gz 
+#tar xf bedtools-2.27.1.tar.gz 
+#rm bedtools-2.27.1.tar.gz
+#cd bedtools2
+#make
+#cp bin/bedtools bin/intersectBed ${cwd}/bin/
+#cd ${cwd}
 
+cd downloads
+xpip=`which pip`
+python3 ${xpip} install snakemake
 cd ${cwd}
-
-pip3 --version
