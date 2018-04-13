82cdc0bac15f6af418364d1fdf814017c5bba867
dariober <dario.beraldi@gmail.com>
2018-04-13 15:00:33 +0100

    Test R packages

--- install/install_pkgs.R
+++ install/install_pkgs.R
@@ -3,3 +3,6 @@
 # userdir <- unlist(strsplit(Sys.getenv("R_LIBS_USER"), .Platform$path.sep))[1L]
 
 install.packages('data.table', repos='http://cran.r-project.org')
+install.packages('argparse', repos='http://cran.r-project.org')
+install.packages('devtools', repos='http://cran.r-project.org')
+devtools::install_github("mskcc/facets", build_vignettes = TRUE)
