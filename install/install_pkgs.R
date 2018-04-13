0ccf8096924a8b8e3cc7e1790bd2d672cce4e8c0
dariober <dario.beraldi@gmail.com>
2018-04-13 15:40:41 +0100

    Test R packages

--- install/install_pkgs.R
+++ install/install_pkgs.R
@@ -1,7 +1,5 @@
 #!/usr/bin/env Rscript
 
-# userdir <- unlist(strsplit(Sys.getenv("R_LIBS_USER"), .Platform$path.sep))[1L]
-
 install.packages('data.table', repos='http://cran.r-project.org')
 install.packages('argparse', repos='http://cran.r-project.org')
 
