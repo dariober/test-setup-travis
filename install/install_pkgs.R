1082c33f3ecb568c671336bc4eceacf748f09a32
dariober <dario.beraldi@gmail.com>
2018-04-13 15:30:36 +0100

    Test R packages

--- install/install_pkgs.R
+++ install/install_pkgs.R
@@ -4,5 +4,9 @@
 
 install.packages('data.table', repos='http://cran.r-project.org')
 install.packages('argparse', repos='http://cran.r-project.org')
+
+system('git clone https://github.com/mskcc/pctGCdata.git downloads/pctGCdata')
+install.packages("downloads/pctGCdata", repos= NULL, type= "source")
+
 system('git clone https://github.com/mskcc/facets.git downloads/facets')
 install.packages("downloads/facets", repos= NULL, type= "source", build_vignettes= TRUE)
