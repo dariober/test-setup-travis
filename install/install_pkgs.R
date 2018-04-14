f68b617f4477eb0250ef598275ccec79eb05d078
dariober <dario.beraldi@gmail.com>
2018-04-14 17:07:16 +0100

    Python3 and pip3 fixed. Testing R

--- install/install_pkgs.R
+++ install/install_pkgs.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env Rscript
 
+install.packages('digest', repos='http://cran.r-project.org')
 install.packages('data.table', repos='http://cran.r-project.org')
 install.packages('argparse', repos='http://cran.r-project.org')
 install.packages('devtools', repos='http://cran.r-project.org')
