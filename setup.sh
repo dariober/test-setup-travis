de47806a81683e4ae438bfbc9cc585cd745c6280
dariober <dario.beraldi@gmail.com>
2018-04-16 09:17:06 +0100

    Testing perl/VEP

--- setup.sh
+++ setup.sh
@@ -4,93 +4,128 @@ VERSION=0.1.0
 
 set -e
 set -o pipefail
-set -x 
 
-if [[ $1 == '-h' || $1 == '--help' ]]
+# Parse arguments
+# ===============
+
+PG=`basename "$0"`
+bin_dir=${HOME}/bin
+
+# From https://stackoverflow.com/questions/192249/how-do-i-parse-command-line-arguments-in-bash
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -b|--bin_dir)
+        bin_dir="$2"
+        shift # past argument
+        shift # past value
+    ;;
+    -v|--version)
+        version=1
+        shift # past argument
+    ;;
+    -h|--help)
+        help=1
+        shift # past argument
+    ;;
+    *)
+        echo "Unknown option in $@"  
+        exit 1
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+
+if [[ $help == 1 ]]
 then
 cat <<EOF
 DESCRIPTION
 Install script
 
-USAGE
-bash setup.sh [--user]
+-b|--bin_dir  Install missing programs in this directory. Default $bin_dir
+-v|--version  Show version
+-h|--help     Show help
 
 Version $VERSION
 EOF
 exit 0
 fi
 
-if [[ $1 == '-v' || $1 == '--version' ]]
+if [[ $version == 1 ]]
 then
-    echo $VERSION
+    echo "$PG $VERSION"
     exit 0
 fi
 
-user=0
-for x in "$@"
-do
-   if [[ "$x" == "--user" ]]
-   then
-       user=1
-   fi
-done
+set -x 
+# ==========
 
 cwd=`pwd`
 mkdir -p downloads
-mkdir -p bin
+
+# Snakemake
+found=`command -v snakemake` || true
+if [[ -z $found ]]
+then
+    pip3 install -user snakemake
+fi
+command -v snakemake
+snakemake --version
 
 # VEP
-cd ${cwd}/downloads
-export PERL_MM_USE_DEFAULT=1
-wget https://github.com/Ensembl/ensembl-vep/archive/release/92.1.tar.gz
-tar xf 92.1.tar.gz
-rm 92.1.tar.gz
-cd ensembl-vep-release-92.1
-perl INSTALL.pl --NO_HTSLIB --AUTO a 
-ln -sf `pwd`/vep ${cwd}/bin/
-cd ${cwd}
-bin/vep --help
-bin/vep --database --vcf -i downloads/ensembl-vep-release-92.1/examples/homo_sapiens_GRCh38.vcf -o test.vep.vcf
-rm test.vep.vcf*
+found=`command -v vep` || true
+if [[ -z $found ]]
+then
+    cd ${cwd}/downloads
+    export PERL_MM_USE_DEFAULT=1
+    wget https://github.com/Ensembl/ensembl-vep/archive/release/92.1.tar.gz
+    tar xf 92.1.tar.gz
+    rm 92.1.tar.gz
+    cd ensembl-vep-release-92.1
+    perl INSTALL.pl --NO_HTSLIB --AUTO a 
+    ln -sf `pwd`/vep ${bin_dir}/
+fi
+command -v vep
+vep --help
 
-#########
+# Samtools
+found=`command -v samtools` || true
+if [[ -z $found ]]
+then
+    cd ${cwd}/downloads
+    wget https://github.com/samtools/samtools/releases/download/1.8/samtools-1.8.tar.bz2
+    tar xf samtools-1.8.tar.bz2
+    rm samtools-1.8.tar.bz2
+    cd samtools-1.8
+    ./configure --prefix=`pwd`
+    make
+    make install
+    cp samtools ${bin_dir}/
+fi
+command -v samtools
+samtools --version
+
+# bedtools
+found=`command -v bedtools` || true
+if [[ -z $found ]]
+then
+    cd ${cwd}/downloads
+    wget https://github.com/arq5x/bedtools2/releases/download/v2.27.1/bedtools-2.27.1.tar.gz 
+    tar xf bedtools-2.27.1.tar.gz 
+    rm bedtools-2.27.1.tar.gz
+    cd bedtools2
+    make
+    cp bin/* ${bin_dir}/
+fi
+command -v bedtools
+bedtools --version
+
 exit 0
-#########
 
 # R packages
 cd ${cwd}
 Rscript install/install_pkgs.R
-
-# Snakemake
-if [[ $user == 1 ]]
-then
-    pip3 install --user snakemake
-else
-    pip3 install snakemake
-fi
-
-# Samtools
-cd ${cwd}/downloads
-wget https://github.com/samtools/samtools/releases/download/1.8/samtools-1.8.tar.bz2
-tar xf samtools-1.8.tar.bz2
-rm samtools-1.8.tar.bz2
-cd samtools-1.8
-./configure --prefix=`pwd`
-make
-make install
-cp samtools ${cwd}/bin/
-cd ${cwd}
-bin/samtools --version
-
-# bedtools
-cd ${cwd}/downloads
-wget https://github.com/arq5x/bedtools2/releases/download/v2.27.1/bedtools-2.27.1.tar.gz 
-tar xf bedtools-2.27.1.tar.gz 
-rm bedtools-2.27.1.tar.gz
-cd bedtools2
-make
-cp bin/bedtools bin/intersectBed ${cwd}/bin/
-cd ${cwd}
-bin/bedtools --version
-
 
